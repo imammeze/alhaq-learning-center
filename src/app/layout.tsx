76b7b7180a57bc0fd8e41286968754360a17f612
Muhammad Imam Al Amin <imamalamin485@gmail.com>
2026-06-11 13:02:58 +0700

    feat: Refactor layout component by removing unused Geist_Mono font and updating metadata title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -7,14 +7,9 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
-  title: "Alhaq Learning Center",
-  description: "Alhaq Learning Center adalah pusat pendidikan dan bimbingan belajar yang beralamat di Bata Merah, Purwokerto. Kami menyediakan berbagai program edukasi untuk kelompok usia dini hingga dewasa, dengan fokus pada pendidikan umum dan nilai-nilai spiritual Islam.",
+  title: "Batamerah Learning Center",
+  description: "Batamerah Learning Center adalah pusat pendidikan dan bimbingan belajar yang beralamat di Bata Merah, Purwokerto. Kami menyediakan berbagai program edukasi untuk kelompok usia dini hingga dewasa, dengan fokus pada pendidikan umum dan nilai-nilai spiritual Islam.",
 };
 
 export default function RootLayout({
@@ -25,7 +20,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
